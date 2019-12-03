1d5e1e2f9ba44e049987cbb2132633b04668cd82
MooseMan <ssaciolo@gmail.com>
2019-12-03 18:56:20 -0500

    10 listeners plus stretch

--- js/index.js
+++ js/index.js
@@ -1,18 +1,40 @@
 // Your code goes here
-const links = document.querySelectorAll('a')
 
 // Looping through links and adding event listener to each one
+const links = document.querySelectorAll('a')
+
 links.forEach(element => {
+  // 1. mouseenter
   element.addEventListener('mouseenter', () => {
     element.style.transform = 'scale(1.5)';
     element.style.transition = 'transform 0.3'
     element.style.textDecoration = 'underline'
   })
+  // 2. mouseleave
   element.addEventListener('mouseleave', () => {
     element.style.transform = 'scale(1)'
     element.style.textDecoration = 'none'
   })
 });
+
+// Adding flip  to image (stretch goal)
+const introsec = document.querySelector('.intro').addEventListener("contextmenu", (event) => {
+  gsap.to('.intro', {
+    duration: 1,
+    rotateY: 180,
+    ease: "elastic(1, 0.75)"
+
+
+  })
+  event.stopPropagation();
+}) 
+
+// Adding resize
+const viewPort = document.querySelector('body');
+window.addEventListener('resize', () => {
+ viewPort.style.backgroundColor = 'lightblue'
+}); 
+
 
 // Adding image on click
 const blownUpBus = document.querySelector('img')
@@ -41,20 +63,45 @@ blownUpBus.addEventListener('click', e => {
   }
 })
 
-//change to dark mode with keydown
-const background = document.querySelector('*');
+// Adding Dark mode with keydown
+const background = document.querySelector('body');
+const header = document.querySelector('.main-navigation');
+const footer = document.querySelector('footer');
+
 document.addEventListener('keydown', (event) => {
-  if (background.style.backgroundColor != 'black') {
+  if(background.style.backgroundColor != 'black'){
     background.style.backgroundColor = 'black';
     background.style.color = 'whitesmoke';
   }
+  if(header.style.backgroundColor != 'black'){
+    header.style.backgroundColor = 'black';
+    header.style.color = 'whitesmoke';
+  }
+  if(footer.style.backgroundColor != 'black'){
+    footer.style.backgroundColor = 'black';
+    footer.style.color = 'whitesmoke';
+  }
 })
-
 
 //change back to light mode with keypress
 document.addEventListener('keypress', (event) => {
-  if (background.style.backgroundColor != 'white') {
+  if(background.style.backgroundColor != 'white'){
     background.style.backgroundColor = 'white';
     background.style.color = 'black';
   }
-}) 
+  if(header.style.backgroundColor != 'white'){
+    header.style.backgroundColor = 'white';
+    header.style.color = 'black';
+  }
+  if(footer.style.backgroundColor != 'white'){
+    footer.style.backgroundColor = 'white';
+    footer.style.color = 'black';
+  }
+})
+
+window.addEventListener('wheel', () => {
+  viewPort.style.backgroundColor = 'yellow'
+})
+
+
+
